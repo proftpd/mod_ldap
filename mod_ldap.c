eece69c047ef0ccc81a6f6c28dd07b7d3a62c582
John Morrissey <jwm@horde.net>
2007-09-07 12:11:34 -0400

    Fix anonymous binds when draft-ietf-ldapext-ldap-c-api-04 functions are used.
    Submitted by: Quentin Garnier <cube@cubidou.net>

--- mod_ldap.c
+++ mod_ldap.c
@@ -298,7 +298,7 @@ pr_ldap_connect(LDAP **conn_ld, int bind)
   if (bind == TRUE) {
 #if LDAP_API_VERSION >= 2000
     bindcred.bv_val = ldap_dnpass;
-    bindcred.bv_len = strlen(ldap_dnpass);
+    bindcred.bv_len = ldap_dnpass != NULL ? strlen(ldap_dnpass) : 0;
     ret = ldap_sasl_bind_s(*conn_ld, ldap_dn, NULL, &bindcred, NULL, NULL, NULL);
 #else /* LDAP_API_VERSION >= 2000 */
     ret = ldap_simple_bind_s(*conn_ld, ldap_dn, ldap_dnpass);
