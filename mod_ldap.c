fed88719626754e36eaba70b756366360084e1e2
John Morrissey <jwm@horde.net>
2012-07-09 16:41:52 -0700

    update copyright date

--- mod_ldap.c
+++ mod_ldap.c
@@ -1,6 +1,6 @@
 /*
  * mod_ldap - LDAP password lookup module for ProFTPD
- * Copyright (c) 1999-2011, John Morrissey <jwm@horde.net>
+ * Copyright (c) 1999-2012, John Morrissey <jwm@horde.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
